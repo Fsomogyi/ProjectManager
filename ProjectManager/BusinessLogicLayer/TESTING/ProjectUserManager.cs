9f7f94d1d4bb88cdf8e0482461e12f8d2e49dcd8
Fsomogyi <ferencsomogyi92@gmail.com>
2015-11-18 20:00:37 +0100

    EF leképzés

---
+++ ProjectManager/BusinessLogicLayer/TESTING/ProjectUserManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.TESTING
+{
+    public class ProjectUserManager
+    {
+        public List<ProjectUserData> GetAllProjectUserData()
+        {
+            using (var context = new ProjectManagerDBEntities())
+            {
+                var allProjectUsers = (from pu in context.ProjectUser
+                                       select new ProjectUserData
+                                       {
+                                           Id = pu.id,
+                                           Name = pu.username,
+                                           Password = pu.password,
+                                           Email = pu.email
+                                       }).ToList();
+
+                return allProjectUsers;
+            }
+        }
+    }
+}
